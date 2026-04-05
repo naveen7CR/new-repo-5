be71597eca66b4040e2aaf9b6b17260a2576c7ef
Naveen Singh <naveenshubham620@gmail.com>
2026-04-05 19:41:11 +0530

    sixth commit

---
+++ random quote.py
@@ -0,0 +1,10 @@
+import random
+
+quotes = [
+    "Stay hungry, stay foolish.",
+    "Code is like humor.",
+    "Fix the cause, not the symptom.",
+    "Simplicity is the soul of efficiency."
+]
+
+print(random.choice(quotes))
