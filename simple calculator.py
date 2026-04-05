8028321ba4f7ac91aaa7471ac26d07b2ba95b46b
Naveen Singh <naveenshubham620@gmail.com>
2026-04-05 19:40:24 +0530

    fifth commit

---
+++ simple calculator.py
@@ -0,0 +1,17 @@
+def calculator():
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    op = input("Enter operation (+, -, *, /): ")
+
+    if op == '+':
+        print("Result:", a + b)
+    elif op == '-':
+        print("Result:", a - b)
+    elif op == '*':
+        print("Result:", a * b)
+    elif op == '/':
+        print("Result:", a / b)
+    else:
+        print("Invalid operation")
+
+calculator()
