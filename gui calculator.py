06636a9a4be825371b271b5d299af9b9534f9c90
Naveen Singh <naveenshubham620@gmail.com>
2026-04-07 13:17:36 +0530

    eight commit

--- gui calculator.py
+++ gui calculator.py
@@ -40,3 +40,63 @@ for b in buttons:
         row += 1
 
 root.mainloop()
+
+
+
+class DoublyLinkedListNode:
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, value):
+        new_node = DoublyLinkedListNode(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+
+    def prepend(self, value):
+        new_node = DoublyLinkedListNode(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+
+    def delete(self, value):
+        current = self.head
+        while current:
+            if current.value == value:
+                if current.prev:
+                    current.prev.next = current.next
+                else:
+                    self.head = current.next
+                if current.next:
+                    current.next.prev = current.prev
+                else:
+                    self.tail = current.prev
+                return
+            current = current.next
+
+
+class Node:
+    def __init__(self,Data):
+        self.prev=None
+        self.next=None
+        self.data=Data
+        curnode=self.head
+        while curnode.next!=None:
+            print(curnode.data,end=" ")
+            curnode=curnode.next
+            
